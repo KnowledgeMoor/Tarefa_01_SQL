3bd78db8da0e35cf10319c7ab0bbadb51eed1c13
Gabriel <131317539+KnowledgeMoor@users.noreply.github.com>
2024-03-07 21:38:38 -0300

    Add files via upload

---
+++ src/main/java/com/example/sql_tarefa/SqlTarefaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sql_tarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlTarefaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sql_tarefa/SqlTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sql_tarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
